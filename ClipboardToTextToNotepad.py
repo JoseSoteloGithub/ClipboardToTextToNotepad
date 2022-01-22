e36ace040655c789ad17d7f10f02226382d3b77b
JoseSoteloGithub <josegsotelo@yahoo.com>
2022-01-22 15:37:11 -0700

    Create ClipboardToTextToNotepad.py

---
+++ ClipboardToTextToNotepad.py
@@ -0,0 +1,44 @@
+import cv2 
+import pytesseract
+from PIL import ImageGrab
+import pyperclip
+import time
+from pywinauto import application
+
+pytesseract.pytesseract.tesseract_cmd =  r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+app = application.Application()
+app.start("Notepad.exe")
+
+recent_value = ''
+while True:
+    try:
+        img_grab = ImageGrab.grabclipboard()
+
+        img_grab_repr = repr(img_grab)
+        size_index = img_grab_repr.find('size')
+        size_index_end = img_grab_repr.find(' ', size_index)
+        img_grab_repr = img_grab_repr[:size_index_end]
+
+        if (not img_grab is None) and (img_grab_repr != recent_value):
+            print('running from image')
+            recent_value = img_grab_repr
+
+            img_grab.save('somefile.png','PNG')
+            img_grab = cv2.imread('somefile.png')
+
+            # Adding custom options
+            custom_config = r'--oem 3 --psm 6'
+            result = pytesseract.image_to_string(img_grab, config=custom_config)
+            
+            app['Notepad']['Edit'].set_edit_text(result)
+        text_grab = pyperclip.paste()
+        if (text_grab != '') and (text_grab != recent_value):
+            print('running from text')
+            result = pyperclip.paste()
+            
+            recent_value = result
+            app['Notepad']['Edit'].set_edit_text(result)
+    except:
+        pass
+    time.sleep(0.1)
